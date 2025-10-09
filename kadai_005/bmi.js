de2e472c561c8790a6be12534f8852692270ad1a
se1001a428 <se1001.a428@icloud.com>
2025-10-09 22:45:32 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,19 @@
+// 変数の宣言
+let weight = 68;
+
+// コンソールへの出力
+console.log(weight);
+
+let height = 1.7;
+
+// コンソールへの出力
+console.log(height);
+
+let bmi = (weight / (height * height));
+
+console.log(bmi);
+
+
+// 四則演算
+console.log(weight / (height * height));
+
