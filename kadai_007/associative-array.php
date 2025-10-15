e93f9887d38e8bee2ca05ccee9782e143768a802
se1001a428 <se1001.a428@icloud.com>
2025-10-16 01:02:29 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,12 @@
+<?php
+$personal_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+print_r($personal_data);
+
+echo $personal_data['name'];
+
+echo $personal_data['price'];
+
+echo $personal_data['weight'];
+
+?>
